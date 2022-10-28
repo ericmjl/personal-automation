00ce820ac53298405aceb308573837bbe2d2d55a
Eric Ma <ericmajinglong@gmail.com>
2022-10-27 23:07:22 -0400

    Update CLI with stitch-videos command.

--- personal_automation/cli.py
+++ personal_automation/cli.py
@@ -1,6 +1,6 @@
 """Personal automation CLI."""
 from pathlib import Path
-from .audio import stitch
+from personal_automation import audio, movie
 from pydub import AudioSegment
 from loguru import logger
 
@@ -21,15 +21,50 @@ def stitch_audio(
     glob_pattern="*.WAV",
     directory: Path = Path("."),
 ):
+    """Stitch audio files together.
+
+    :param output_path: File path to output file.
+    :param glob_pattern: Pattern of files to look for.
+    :param directory: Where to search for WAV files.
+    :raises ValueError: if we cannot find any files matching the glob pattern.
+    """
     audio_files = sorted(directory.glob(glob_pattern))
     if len(audio_files) == 0:
         raise ValueError(
-            f"No audio files matching the glob pattern {glob_pattern} was found! "
-            f"These are the files that are present: {sorted(f for f in Path(directory).iterdir() if f.is_file())}"
+            "No audio files matching the glob pattern "
+            f"{glob_pattern} was found! "
+            "These are the files that are present: "
+            f"{sorted(f for f in Path(directory).iterdir() if f.is_file())}"
         )
     logger.info(f"Stitching together the following audio files: {audio_files}")
-    stitched_audio: AudioSegment = stitch(audio_files)
+    stitched_audio: AudioSegment = audio.stitch(audio_files)
     stitched_audio.export(output_path)
+
+
+@app.command()
+def stitch_video(
+    output_path: Path = Path("stitched.mov"),
+    glob_pattern="*.MOV",
+    directory: Path = Path("."),
+):
+    """Stitch video files together.
+
+    :param output_path: File path to output file.
+    :param glob_pattern: Pattern of files to look for.
+    :param directory: Where to search for WAV files.
+    :raises ValueError: if we cannot find any files matching the glob pattern.
+    """
+    video_files = sorted(directory.glob(glob_pattern))
+    if len(video_files) == 0:
+        raise ValueError(
+            "No video files matching the glob pattern "
+            f"{glob_pattern} was found! "
+            "These are the files that are present: "
+            f"{sorted(f for f in Path(directory).iterdir() if f.is_file())}"
+        )
+    logger.info(f"Stitching together the following video files: {video_files}")
+    stitched_video = movie.stitch(video_files)
+    stitched_video.write_videofile(output_path)
 
 
 if __name__ == "__main__":
--- personal_automation/movie.py
+++ personal_automation/movie.py
@@ -1,1 +1,18 @@
 """High-level functions to do stuff with movies."""
+from moviepy.editor import concatenate_videoclips, VideoFileClip, VideoClip
+from typing import List
+from pathlib import Path
+
+
+def stitch(fpaths: List[Path]) -> VideoClip:
+    """Stitch together video clips.
+
+    :param fpaths: List of file paths to video files to stitch.
+    :returns: A concatenated VideoClip object.
+    """
+    clips = []
+    for fpath in fpaths:
+        clip = VideoFileClip(str(fpath))
+        clips.append(clip)
+    final: VideoClip = concatenate_videoclips(clips)
+    return final
